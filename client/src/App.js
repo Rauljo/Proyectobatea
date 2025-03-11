1a424d7cc5ab00851c37483daed8ad0fd797f9d7
Rauljo <raulsal2002@gmail.com>
2025-03-11 12:37:59 +0100

    added the option to add bateas

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import React, {Fragment} from 'react';
 import './App.css';
 
 import Selector_Menu from './components/Selector_Menu.js';
+import Nueva_Batea from './components/Nueva_Batea.js';
 
 function App() {
   return (
     <Fragment>
       <h1>APP BATEAS</h1>
       <Selector_Menu />
+      <Nueva_Batea />
     </Fragment>
   );
 }
---
+++ client/src/components/Nueva_Batea.js
@@ -0,0 +1,17 @@
+import React, {Fragment, useState, useEffect} from "react";
+import axios from "axios";
+
+//Material UI
+import FormDialog from "./Form_Dialog";
+
+const Nueva_Batea = () => {
+    return (
+        <Fragment>
+            <h2>Nueva Batea</h2>
+            <FormDialog />
+        </Fragment>
+    );
+};
+
+export default Nueva_Batea;
+
