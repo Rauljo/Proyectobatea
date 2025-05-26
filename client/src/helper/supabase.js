bce3bb80828d0192dbc3989293d1a1f5afc3af59
Rauljo <raulsal2002@gmail.com>
2025-05-27 00:18:17 +0200

    process

--- client/src/helper/supabase.js
+++ client/src/helper/supabase.js
@@ -1,7 +1,7 @@
 import {createClient} from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://wpcbligedufvtiwugaso.supabase.co';
-const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
+const supabaseAnonKey = process.env.PUBLIC_SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
