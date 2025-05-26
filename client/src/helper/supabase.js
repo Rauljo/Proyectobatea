df7e8c7d04d01525830130f0dbe3aec671aaef1a
Rauljo <raulsal2002@gmail.com>
2025-05-27 00:21:22 +0200

    subscription_2

--- client/src/helper/supabase.js
+++ client/src/helper/supabase.js
@@ -1,7 +1,7 @@
 import {createClient} from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://wpcbligedufvtiwugaso.supabase.co';
-const supabaseAnonKey = process.env.PUBLIC_SUPABASE_ANON_KEY;
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndwY2JsaWdlZHVmdnRpd3VnYXNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc4MjgxMTQsImV4cCI6MjA2MzQwNDExNH0.TW2uSmrlSxkbMCO1wn0g0d0RVMAXHy2Hh3aGUBxz-O0";
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
