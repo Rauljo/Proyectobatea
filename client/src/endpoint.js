abe08d405168f20912e2dadfc54812ed23dcde10
Rauljo <raulsal2002@gmail.com>
2025-11-15 18:14:47 +0100

    Change endpoint

--- client/src/endpoint.js
+++ client/src/endpoint.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 export const BASE_ENDPOINT = process.env.REACT_APP_BASE_ENDPOINT
 export const PORTS = process.env.REACT_APP_PORTS
-=======
-export const BASE_ENDPOINT = "https://proyectobatea.onrender.com"
-export const PORTS = 5010
->>>>>>> 22f93e588d1272c90e264f85a09ce181ebe50411
 
 export const BASE_ROUTE = `${BASE_ENDPOINT}:${PORTS}`
 
