a15d791628dacae4e21fa6e2ec00009ec47577d4
Rauljo <raulsal2002@gmail.com>
2025-12-29 13:46:33 +0100

    Local_database

--- server/db.js
+++ server/db.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-    user: 'postgres.wpcbligedufvtiwugaso',
+    user: process.env.SUPABASE_USER || 'postgres.wpcbligedufvtiwugaso',
     password: process.env.DB_PASSWORD,
-    host: 'aws-0-eu-west-3.pooler.supabase.com',
-    port: 5432,
+    host: process.env.SUPABASE_HOST || 'aws-0-eu-west-3.pooler.supabase.com',
+    port: process.env.SUPABASE_PORT || 5432,
     database: 'postgres',
     ssl: {rejectUnauthorized: false}
 });
